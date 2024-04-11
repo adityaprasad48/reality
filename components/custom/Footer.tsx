100b4e49bd9a61935eb013470819cabef5e24db9
Aditya Prasad <adityaprasad20799@gmail.com>
2024-04-11 20:51:50 +0530

    Header fix

--- components/custom/Footer.tsx
+++ components/custom/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="min-h-20 w-full border-t-2 border-slate-200 shadow-sm px-4">
+    <footer className="min-h-[65px] w-full border-t-2 border-slate-200 shadow-sm px-4">
       <div className="lg:max-w-screen-lg h-full mx-auto flex items-center justify-between">
         <div className="w-full flex justify-center cursor-pointer">
           Made with ❤️ by &nbsp; <Link  className="text-slate-400 hover:text-slate-600 hover:underline" href="https://github.com/adityaprasad48" target="_blank">adityaprasad48</Link>
